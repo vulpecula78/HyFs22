00271fa12eb18b81a73e0bc68c52ff5c689b8974
Tino Pönni <tino.ponni@tutanota.com>
2022-06-10 10:06:21 +0300

    osa1 tehtävät 1.1 - 1.6

---
+++ osa1/unicafe/src/App.js
@@ -0,0 +1,46 @@
+import { useState } from 'react'
+
+const Button = ({handleClick, text}) => (
+  <button onClick  = {handleClick}>
+	{text}
+  </button>  
+)
+
+const App = () => {
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+  const [all, setAll] = useState(0)
+  
+  const handleGoodClick = () => (
+    setGood(good + 1)
+  )
+  
+  const handleNeutralClick = () => (
+    setNeutral(neutral + 1)
+  )
+
+  const handleBadClick = () => (
+    setBad(bad + 1)
+  )
+  
+  return (
+    <div>
+	  <h1>Give feedback</h1>
+	  <div>
+		<Button handleClick={handleGoodClick} text='good' />
+		<Button handleClick={handleNeutralClick} text='neutral' />
+		<Button handleClick={handleBadClick} text='bad' />
+	  </div>
+      <h1>statistics</h1>
+	  <p>
+        good {good} <br />
+		neutral {neutral} <br />
+	    bad {bad} <br />
+		all {all} <br />
+	  </p>
+    </div>
+  )
+}
+
+export default App;
