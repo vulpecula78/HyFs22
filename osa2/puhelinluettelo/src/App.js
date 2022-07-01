bf44a1627dba292d792784b8142f7c70482555af
Tino Pönni <tino.ponni@tutanota.com>
2022-07-01 12:35:56 +0300

    Tehtävä 2.8

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -3,26 +3,34 @@ import { useState } from 'react'
 const Person = ( {person} ) => {
   return (
     <div>
-      {person.name}
+      {person.name} {person.number}
     </div>
   )
 }
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', number: '040-123 1234' },
+    { name: 'Konrad Zuse', number: '+44 50 554 3444' }
   ]) 
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
   
   const addNewName = (event) => {
     setNewName(event.target.value)
     console.log(event.target)
   }
-  
+
+  const addNewNumber = (event) => {
+    setNewNumber(event.target.value)
+    console.log(newNumber)
+  }
+
   const addName = (event) => {
     event.preventDefault()
     const nameObject = {
-      name: newName
+      name: newName,
+      number: newNumber
     }
 
     if (persons.find(( {name} ) =>
@@ -31,6 +39,7 @@ const App = () => {
       } else {
         setPersons(persons.concat(nameObject))
         setNewName('')
+        setNewNumber('')
       }
   }
 
@@ -40,6 +49,9 @@ const App = () => {
       <form onSubmit={addName}>
         <div>
           name: <input value={newName} onChange={addNewName} />
+        </div>
+        <div>
+          number: <input value={newNumber} onChange={addNewNumber} />
         </div>
         <div>
           <button type="submit">add</button>
